8da09367e463e5fccab7555be0d84aaa8e1649fe
Xie-LC123 <teamsevdekoliang@gmail.com>
2025-11-28 23:06:57 +0800

    Update script.js

--- 4 - Modifying DOM elements/Challenge/Challenge/script.js
+++ 4 - Modifying DOM elements/Challenge/Challenge/script.js
@@ -1,7 +1,13 @@
 function changeStyle() {
-    // Challenge Instructions:
-    // 1. When the button is clicked, change the background color of 'dynamicText' to 'lightblue'.
-    // 2. Change the font size of 'dynamicText' to '18px'.
-    // 3. Toggle the font weight of 'dynamicText' between 'normal' and 'bold' on each button click.
-    // 4. Change the text color of 'dynamicText' to 'green'.
+    let isBold = false;
+    let dynamicText = document.getElementById('dynamicText');
+
+    dynamicText.style.backgroundColor = 'lightblue';
+
+    dynamicText.style.fontSize = '18px';
+
+    dynamicText.style.fontWeight = isBold ? 'normal' : 'bold';
+    isBold = !isBold;
+
+    dynamicText.style.color = 'green';
 }
